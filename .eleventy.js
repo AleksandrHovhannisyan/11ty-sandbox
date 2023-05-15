29ba3d0479dcbb499d2577712a426429eadab509
AleksandrHovhannisyan <aleksandrhovhannisyan@gmail.com>
2023-05-15 18:36:01 -0500

    Demo lightningcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
-const TEMPLATE_ENGINE = 'liquid';
+const pluginLightningCSS = require('@11tyrocks/eleventy-plugin-lightningcss');
 
 module.exports = (eleventyConfig) => {
-  return {
-    dir: {
-        input: 'src',
-        output: '_site',
-        includes: '_includes',
-        layout: '_layouts'
-    },
-    dataTemplateEngine: TEMPLATE_ENGINE,
-    markdownTemplateEngine: TEMPLATE_ENGINE,
-    htmlTemplateEngine: TEMPLATE_ENGINE,
-    templateFormats: ['html', 'md', TEMPLATE_ENGINE],
-  };
+  eleventyConfig.addPlugin(pluginLightningCSS);
+  return {};
 };
